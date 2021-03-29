76a9ad0c95ebd092a75f17522524ffe604902e60
Raphael Vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2021-03-29 06:17:14 -0300

    Creating SignIn button

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,19 @@
+import styles from "./styles.module.scss";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi"
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Raphael Vasconcelos
+      <FiX color="#737380" className={styles.closedIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in with Github
+    </button>
+  );
+}
